ad35ed0a7455b05bae2ed076624647dda654f67a
tonisalas <tonisalas@gmail.com>
2021-02-21 09:14:01 +0100

    Created statistics web project

---
+++ SemboWebStatistics/src/main/java/com/Sembo/web/app/SemboWebStatisticsApplication.java
@@ -0,0 +1,13 @@
+package com.Sembo.web.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemboWebStatisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemboWebStatisticsApplication.class, args);
+	}
+
+}
---
+++ SemboWebStatistics/src/test/java/com/Sembo/web/app/SemboWebStatisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sembo.web.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemboWebStatisticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
